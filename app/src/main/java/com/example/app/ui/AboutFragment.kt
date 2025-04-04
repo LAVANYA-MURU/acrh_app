c48f0b9bf10f25135d966975e0046d58e7ea62b7
LAVANYA-MURU <sec23it104@sairamtap.edu.in>
2025-04-04 11:39:42 +0530

    Update AboutFragment.kt

--- app/src/main/java/com/example/app/ui/AboutFragment.kt
+++ app/src/main/java/com/example/app/ui/AboutFragment.kt
@@ -11,7 +11,8 @@ class AboutFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 }
